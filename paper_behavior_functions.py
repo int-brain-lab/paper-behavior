cd59c7480ed6c9dddf0053a8d630dd05a431e357
Guido Meijer <gtmeijer@gmail.com>
2019-09-17 11:33:50 +0100

    Only include mice that reached trained before end of September

--- paper_behavior_functions.py
+++ paper_behavior_functions.py
@@ -9,6 +9,7 @@ Functions for behavioral paper analysis
 
 from ibl_pipeline import subject, acquisition
 import seaborn as sns
+from os.path import join
 from ibl_pipeline.analyses import behavior as behavior_analysis
 
 
@@ -21,16 +22,21 @@ def query_subjects(as_dataframe=False):
     as_dataframe: boolean if true returns a pandas dataframe (default is False)
     """
 
+    # Query all subjects with project ibl_neuropixel_brainwide_01 and get the date at which
+    # they were flagged as trained_1a
+    subj_query = (subject.Subject * subject.SubjectLab * subject.SubjectProject
+                  & 'subject_project = "ibl_neuropixel_brainwide_01"').aggr(
+                          (acquisition.Session * behavior_analysis.SessionTrainingStatus())
+                          & 'training_status="trained_1a"',
+                          'subject_nickname', 'sex', 'subject_birth_date', 'lab_name',
+                          date_trained='min(date(session_start_time))')
+
+    # Select subjects that reached trained_1a criterium before September 30th
     if as_dataframe is True:
-        subjects = (subject.Subject * subject.SubjectLab * subject.SubjectProject
-                    & 'subject_project = "ibl_neuropixel_brainwide_01"').proj(
-                            'subject_nickname', 'sex', 'subject_birth_date', 'lab_name').fetch(
-                                    format='frame')
-        subjects = subjects.reset_index()
+        subjects = (subj_query & 'date_trained < "2019-09-30"').fetch(format='frame')
+        subjects = subjects.sort_values(by=['lab_name']).reset_index()
     else:
-        subjects = (subject.Subject * subject.SubjectLab * subject.SubjectProject
-                    & 'subject_project = "ibl_neuropixel_brainwide_01"').proj(
-                            'subject_nickname', 'sex', 'subject_birth_date', 'lab_name')
+        subjects = (subj_query & 'date_trained < "2019-09-30"')
     return subjects
 
 
@@ -45,7 +51,7 @@ def query_sessions(protocol='all', training_status='all', stable=False, as_dataf
                      'biased': biasedChoiceWorld sessions
                      'training': trainingChoiceWorld sessions
     training_status: string with the following possibilities
-                     'all' (default), 'trained', 'ready for ephys'
+                     'all' (default), 'trained_1a', 'trained_1b', 'ready for ephys'
     stable:          boolean if True only return sessions with stable hardware
                      sessions after July 10, 2019 (default is False)
     as_dataframe:    boolean if True returns a pandas dataframe (default is False)
@@ -76,3 +82,16 @@ def seaborn_style():
     Set seaborn style for plotting figures
     """
     sns.set(style="ticks", context="paper", font_scale=1.2)
+
+
+def save_csv_subject_query(path):
+    """
+    Save a csv file with all subjects of the query
+
+    Parameters
+    ----------
+    path: string with path to where to save file
+    """
+
+    subjects = query_subjects(as_dataframe=True)
+    subjects.to_csv(join(path, 'subjects.csv'))

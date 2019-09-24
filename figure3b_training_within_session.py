567fca41c159fe18a6cb78b8f7b2b9b5b85ee008
Miles Wells <k1o0@3tk.co>
2019-09-24 11:47:34 +0300

    Figure 3b

---
+++ figure3b_training_within_session.py
@@ -0,0 +1,32 @@
+"""
+Disengagement criteria
+Anne Urai, Miles Wells, 2019
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from paper_behavior_functions import *
+import datajoint as dj
+from end_session_criteria import SessionEndCriteria  # TODO load from group_shared
+from ibl_pipeline import behavior, subject, acquisition
+
+
+# ================================= #
+# QUERY AN EXAMPLE MOUSE
+# ================================= #
+
+mouse = 'CSHL_015'
+lab = 'churchlandlab'
+
+subj = subject.Subject & 'subject_nickname = "{}"'.format(mouse)
+sessions = behavior.TrialSet & subj & (acquisition.Session() - 'task_protocol LIKE "%habituation%"')
+
+# ================================================================== #
+# QUERY SESSIONS DETAILS FOR EXAMPLE 3 DAYS
+# ================================================================== #
+
+days = [3, 10, 27]
+sessions = sessions.proj(n_days='DATEDIFF(session_start_time, MIN(session_start_time))')
+end_criterion = sessions * SessionEndCriteria

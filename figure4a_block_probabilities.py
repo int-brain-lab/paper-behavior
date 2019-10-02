c4542764e499c07fb9cc1e0012426849a2f0e90d
Anne Urai <anne.urai@gmail.com>
2019-10-02 17:10:22 -0400

    annotated psychfuncs and block structure for figure 4:

--- figure4a_block_probabilities.py
+++ figure4a_block_probabilities.py
@@ -6,12 +6,16 @@ Anne Urai, CSHL, 2019
 import pandas as pd
 import os
 import seaborn as sns
-from figure_style import seaborn_style
+import numpy as np
+from paper_behavior_functions import *
+from dj_tools import *
 
 # INITIALIZE A FEW THINGS
 seaborn_style()
-figpath = os.path.join(os.path.expanduser('~'), 'Data/Figures_IBL')
+figpath = figpath()
 cmap = sns.diverging_palette(20, 220, n=3, center="dark")
+from ibl_pipeline import reference, subject, action, acquisition, data, behavior
+from IPython import embed as shell  # for debugging
 
 # ================================= #
 # SCHEMATIC OF THE BLOCKS
@@ -31,7 +35,31 @@ fig.set(xticks=[-0, 1], xlim=[-0.5, 1.5],
 for ax, title in zip(fig.axes.flat, ['50/50', '20/80', '80/20']):
     ax.set_title(title)
     ax.set_xticklabels(['Left', 'Right'], rotation=45)
-fig.set_axis_labels('Stimulus side', 'Probability (%)')
-fig.savefig(os.path.join(
-    figpath, "figure1_panel_block_distribution.png"), dpi=600)
-fig.savefig(os.path.join(figpath, "figure1_panel_block_distribution.pdf"))
+fig.set_axis_labels('', 'Probability (%)')
+fig.savefig(os.path.join(figpath, "figure4_panel_block_distribution.png"), dpi=600)
+fig.savefig(os.path.join(figpath, "figure4_panel_block_distribution.pdf"))
+plt.close('all')
+
+# ================================= #
+# EXAMPLE SESSION TIMECOURSE
+# ================================= #
+
+b = (subject.Subject & 'subject_nickname="ibl_witten_06"') * \
+    behavior.TrialSet.Trial * (acquisition.Session & 'session_start_time BETWEEN "2019-04-15" AND "2019-04-16"')
+bdat = b.fetch(order_by='session_start_time, trial_id', format='frame').reset_index()
+behav = dj2pandas(bdat)
+behav = behav.loc[behav['trial_id'] <= 1000]
+
+fig, axes = plt.subplots(ncols=1, nrows=1, figsize=(6, 3))
+rightax = axes.twinx()
+sns.lineplot(x=behav.trial_id, y=behav.probabilityLeft, color='k', ax=rightax, zorder=1)
+sns.scatterplot(x=behav.trial_id, y=behav.signed_contrast, hue=np.sign(behav.signed_contrast), 
+                palette=cmap, marker='.', ax=axes, legend=False, alpha=0.5, 
+                edgecolors='face', zorder=2)
+# fig.set_titles("")
+axes.set(xlim=[-5, 1005], xlabel='Trial number', ylabel='Signed contrast (%)') 
+rightax.set(xlim=[-5,1005], ylabel='P(Left)', yticks=[20,50,80])
+plt.tight_layout()
+fig.savefig(os.path.join(figpath, "figure4_panel_session_course.png"), dpi=600)
+fig.savefig(os.path.join(figpath, "figure4_panel_session_course.pdf"))
+plt.close('all')

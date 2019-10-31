51c3e6c360f3a46d1e637624e44edf551e619990
Guido Meijer <gtmeijer@gmail.com>
2019-10-31 19:08:53 +0000

    Various fixes

--- figure3a_psychfuncs_perlab.py
+++ figure3a_psychfuncs_perlab.py
@@ -25,13 +25,14 @@ seaborn_style()
 figpath = figpath()
 pal = group_colors()
 institution_map, col_names = institution_map()
+col_names = col_names[:-1]
 
 # ================================= #
 # GET DATA FROM TRAINED ANIMALS
 # ================================= #
 
 use_sessions, use_days = query_sessions_around_criterion(criterion='trained',
-                                                         days_from_criterion=[2, 0], 
+                                                         days_from_criterion=[2, 0],
                                                          as_dataframe=False)
 # restrict by list of dicts with uuids for these sessions
 b = use_sessions * subject.Subject * subject.SubjectLab * reference.Lab * \
@@ -58,29 +59,34 @@ behav['institution_name'] = behav.institution_code + ': ' + behav.n_mice.apply(s
 
 # plot one curve for each animal, one panel per lab
 fig = sns.FacetGrid(behav,
-                    col="institution_code", col_wrap=4, col_order=col_names,
-                    sharex=True, sharey=True, aspect=1, hue="subject_uuid")
+                    col="institution_code", col_wrap=7, col_order=col_names,
+                    sharex=True, sharey=True, aspect=0.7, hue="subject_uuid")
 fig.map(plot_psychometric, "signed_contrast", "choice_right",
         "subject_nickname", color='gray', alpha=0.7)
 fig.set_titles("{col_name}")
-for axidx, ax in enumerate(fig.axes.flat[0:-1]):
+for axidx, ax in enumerate(fig.axes.flat):
     ax.set_title(behav.institution_name.unique()[axidx], color=pal[axidx], fontweight='bold')
 
 # overlay the example mouse
 tmpdat = behav[behav['subject_nickname'].str.contains('KS014')]
-plot_psychometric(tmpdat.signed_contrast, tmpdat.choice_right, tmpdat.subject_nickname, 
-                  color='black', ax=fig.axes[-2], legend=False)
-
-# NOW ADD THE GROUP
-ax_group = fig.axes[-1]  # overwrite this empty plot
-for i, inst in enumerate(behav.institution_code.unique()):
-    tmp_behav = behav[behav['institution_code'].str.contains(inst)]
-    plot_psychometric(tmp_behav.signed_contrast, tmp_behav.choice_right,
-                      tmp_behav.subject_nickname, ax=ax_group, legend=False, color=pal[i])
-ax_group.set_title('All labs', color='k', fontweight='bold')
+plot_psychometric(tmpdat.signed_contrast, tmpdat.choice_right, tmpdat.subject_nickname,
+                  color='black', ax=fig.axes[0], legend=False)
 fig.despine(trim=True)
 fig.set_axis_labels('Signed contrast (%)', 'Rightward choice (%)')
 
 fig.savefig(os.path.join(figpath, "figure3a_psychfuncs.pdf"))
 fig.savefig(os.path.join(figpath, "figure3a_psychfuncs.png"), dpi=300)
-plt.close('all')
+
+# Plot all labs
+fig, ax1 = plt.subplots(1, 1, figsize=(4, 4))
+for i, inst in enumerate(behav.institution_code.unique()):
+    tmp_behav = behav[behav['institution_code'].str.contains(inst)]
+    plot_psychometric(tmp_behav.signed_contrast, tmp_behav.choice_right,
+                      tmp_behav.subject_nickname, ax=ax1, legend=False, color=pal[i])
+ax1.set_title('All labs', color='k', fontweight='bold')
+ax1.set(xlabel='Signed contrast (%)', ylabel='Rightward choice (%)')
+seaborn_style()
+plt.tight_layout(pad=2)
+
+fig.savefig(os.path.join(figpath, "figure3a_psychfuncs_all_labs.pdf"))
+fig.savefig(os.path.join(figpath, "figure3a_psychfuncs_all_labs.png"), dpi=300)

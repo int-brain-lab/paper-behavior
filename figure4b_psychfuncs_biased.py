d193b6b5dcbdf29386576ec720987873e84f00eb
Anne Urai <anne.urai@gmail.com>
2019-10-31 14:51:10 -0400

    integrated boxplot for figure 4c into script for 4b

--- figure4b_psychfuncs_biased.py
+++ figure4b_psychfuncs_biased.py
@@ -32,6 +32,35 @@ pal = group_colors()
 institution_map, col_names = institution_map()
 
 # ================================= #
+# PSYCHOMETRIC FUNCTIONS
+# FOR OUR EXAMPLE ANIMAL
+# ================================= #
+
+b = (subject.Subject & 'subject_nickname="KS014"') * \
+    behavior.TrialSet.Trial * \
+    (acquisition.Session & 'task_protocol LIKE "%biased%"')
+bdat = b.fetch(order_by='session_start_time, trial_id',
+               format='frame').reset_index()
+behav = dj2pandas(bdat)
+assert not behav.empty
+
+fig = sns.FacetGrid(behav,
+                    hue="probabilityLeft", palette=cmap,
+                    sharex=True, sharey=True, aspect=1)
+fig.map(plot_psychometric, "signed_contrast", "choice_right", "session_uuid")
+fig.set_axis_labels('Signed contrast (%)', 'Rightward choice (%)')
+fig.ax.annotate('80/20', xy=(-5, 0.6), xytext=(-15, 0.8), color=cmap[0], fontsize=12,
+                arrowprops=dict(facecolor=cmap[0], shrink=0.05), ha='right')
+fig.ax.annotate('20/80', xy=(5, 0.4), xytext=(13, 0.18), color=cmap[2], fontsize=12,
+                arrowprops=dict(facecolor=cmap[2], shrink=0.05))
+fig.despine(trim=True)
+fig.axes[0][0].set_title('Example mouse', fontweight='bold', color='k')
+fig.savefig(os.path.join(figpath, "figure4b_psychfuncs_biased_example.pdf"))
+fig.savefig(os.path.join(
+    figpath, "figure4b_psychfuncs_biased_example.png"), dpi=600)
+plt.close('all')
+
+# ================================= #
 # GET DATA FROM TRAINED ANIMALS
 # ================================= #
 
@@ -64,7 +93,6 @@ behav['institution_name'] = behav.institution_code + ': ' + behav.n_mice.apply(s
 # PSYCHOMETRIC FUNCTIONS
 # one for all labs combined
 # ================================= #
-
 
 fig = sns.FacetGrid(behav,
                     hue="probabilityLeft", palette=cmap,
@@ -119,19 +147,19 @@ behav3['biasshift'] = behav3[20] - behav3[80]
 
 # plot one curve for each animal, one panel per lab
 fig = sns.FacetGrid(behav3,
-                    col="institution_code", col_wrap=4, col_order=col_names,
+                    col="institution_code", col_wrap=7, col_order=col_names,
                     sharex=True, sharey=True, aspect=1, hue="subject_nickname")
 fig.map(plot_chronometric, "signed_contrast", "biasshift",
         "subject_nickname", color='gray', alpha=0.7)
-fig.set_axis_labels('Signed contrast (%)', 'Bias shift ($\Delta$ choice %)')
+fig.set_axis_labels('Signed contrast (%)', '$\Delta$ Rightward choice (%)')
 fig.set_titles("{col_name}")
 for axidx, ax in enumerate(fig.axes.flat[0:-1]):
-    ax.set_title(behav.institution_name.unique()[axidx], color=pal[axidx], fontweight='bold')
+    ax.set_title(sorted(behav.institution_name.unique())[axidx], color=pal[axidx], fontweight='bold')
 
 # overlay the example mouse
 tmpdat = behav3[behav3['subject_nickname'].str.contains('KS014')]
 plot_chronometric(tmpdat.signed_contrast, tmpdat.biasshift, tmpdat.subject_nickname,
-                  color='black', ax=fig.axes[-2], legend=False)
+                  color='black', ax=fig.axes[0], legend=False)
 
 # ADD THE GROUP TO THE FIRST AXIS
 ax_group = fig.axes[-1]  # overwrite this empty plot
@@ -140,8 +168,37 @@ for i, inst in enumerate(behav.institution_code.unique()):
     plot_chronometric(tmp_behav.signed_contrast, tmp_behav.biasshift,
                       tmp_behav.subject_nickname, ax=ax_group, legend=False, color=pal[i])
 ax_group.set_title('All labs', color='k', fontweight='bold')
-fig.set_axis_labels('Signed contrast (%)', 'Bias shift ($\Delta$ choice %)')
+fig.set_axis_labels('Signed contrast (%)', '$\Delta$ Rightward choice (%)')
 fig.despine(trim=True)
-fig.savefig(os.path.join(figpath, "figure4a_biasshift.pdf"))
-fig.savefig(os.path.join(figpath, "figure4a_biasshift.png"), dpi=300)
+fig.savefig(os.path.join(figpath, "figure4c_biasshift.pdf"))
+fig.savefig(os.path.join(figpath, "figure4c_biasshift.png"), dpi=300)
 plt.close('all')
+
+# ================================================================== #
+# Plot behavioral metrics per lab
+# ================================================================== #
+
+bias = behav3.loc[behav3.signed_contrast == 0, :]
+# Add all mice to dataframe seperately for plotting
+bias_all = bias.copy()
+bias_all['institution_code'] = 'All'
+bias_all = bias.append(bias_all)
+
+# Set color palette
+use_palette = [[0.6, 0.6, 0.6]] * len(np.unique(bias['institution_code']))
+use_palette = use_palette + [[1, 1, 0.2]]
+sns.set_palette(use_palette)
+
+# plot
+f, ax1 = plt.subplots(1, 1, figsize=(3.5, 3))
+sns.set_palette(use_palette)
+
+sns.boxplot(y='biasshift', x='institution_code', data=bias_all, ax=ax1)
+ax1.set(ylabel='$\Delta$ Rightward choice (%)\n at 0% contrast', ylim=[0, 51], xlabel='')
+[tick.set_color(pal[i]) for i, tick in enumerate(ax1.get_xticklabels()[:-1])]
+plt.setp(ax1.xaxis.get_majorticklabels(), rotation=40)
+plt.tight_layout(pad=2)
+seaborn_style()
+plt.savefig(os.path.join(figpath, 'figure4e_bias_per_lab.pdf'))
+plt.savefig(os.path.join(figpath, 'figure4e_bias_per_lab.png'), dpi=300)
+

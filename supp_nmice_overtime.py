d6b384f61446f228d897b2902ebe0cc0abd0cad5
Anne Urai <anne.urai@gmail.com>
2020-04-23 08:27:14 -0400

    overview of database growth over time for Matteo

---
+++ supp_nmice_overtime.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Query the number of mice at different timepoints of the pipeline
+@author 22 April 2020, Anne Urai
+"""
+
+from paper_behavior_functions import query_subjects
+from ibl_pipeline import subject, acquisition, reference, behavior
+from ibl_pipeline.analyses import behavior as behavior_analysis
+import pandas as pd
+
+
+dates = ['2019-01-01', '2019-03-01', '2020-01-01', '2020-04-01']
+
+print('All mice in database:')
+for d in dates:
+
+    # which mice were in the database by then?
+    subj_query = subject.Subject * subject.SubjectLab * reference.Lab * \
+        behavior.TrialSet & 'session_start_time < "%s"'%d
+    subj_df = subj_query.fetch(format='frame').reset_index()
+
+    print('%s, %d mice, %d labs, %d choices'%(d, subj_df.subject_uuid.nunique(),
+                                              subj_df.lab_name.nunique(),
+                                              subj_df.n_trials.sum()))
+
+print('Brainwide map project mice:')
+for d in dates:
+
+    # which mice were in the database by then?
+    subj_query = subject.Subject * subject.SubjectLab * reference.Lab \
+                 * (subject.SubjectProject & 'subject_project = "ibl_neuropixel_brainwide_01"') \
+                 * (behavior.TrialSet & 'session_start_time < "%s"'%d)
+    subj_df = subj_query.fetch(format='frame').reset_index()
+
+    print('%s, %d mice, %d labs, %d choices'%(d, subj_df.subject_uuid.nunique(),
+                                              subj_df.lab_name.nunique(),
+                                              subj_df.n_trials.sum()))

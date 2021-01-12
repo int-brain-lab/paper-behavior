bf515847f9bf06fc6e04b44e2c73e0e53fa1a9e0
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2021-01-12 18:17:11 +0100

    Check trained1a/1b not on similar dates

--- text_trained1a_to_1b_sessions.py
+++ text_trained1a_to_1b_sessions.py
@@ -29,20 +29,36 @@ else:
     ses = pd.read_csv(join(datapath(), 'Fig2c.csv'))
     use_subjects = ses['subject_uuid'].unique()  # For counting the number of subjects
 
-ses = ses.sort_values(by=['subject_nickname', 'training_status'])
-uni_sub = np.unique(ses['subject_nickname'])
+ses = ses.sort_values(by=['subject_uuid', 'session_start_time'])
+uni_sub = np.unique(ses['subject_uuid'])
 
-# Construct dataframe
-training_time_ab = pd.DataFrame(columns=['sessions'], data=ses.groupby(['subject_nickname','training_status']).size())
 training_time_b = pd.DataFrame(columns=['sessions'])
 # Loop over subjects
 for i_sub in range(0, len(uni_sub)):
     subj = uni_sub[i_sub]
-    ab = training_time_ab.loc[subj]
-    # Check if both trained_1a and trained_1b are found as keys
-    if len(ab) == 2:  # Assumed ==2 is fine, as there are only the trained_1a/1b labels to be found as per the DJ query
-        tr_b = ab.loc['trained_1b']
-        training_time_b = training_time_b.append(tr_b)
+
+    # # Construct dataframe
+    df = ses.loc[ses['subject_uuid'] == subj]
+    if len(np.unique(df['training_status'])) == 2:  # Append
+
+        # Check that the session start date is different for when reaching 1a/1b
+        df = df.sort_values(by=['session_start_time'])  # Ensure data is sorted by date
+
+        # Find index of relevant session
+        indx_a = np.where(df['training_status'] == 'trained_1a')[0]
+        n_row_a = indx_a[-1]  # last session with trained 1a
+        indx_b = np.where(df['training_status'] == 'trained_1b')[0]
+        n_row_b = indx_b[0]  # first session with trained 1b
+        if n_row_a+1 != n_row_b:
+            print("ERROR")
+        #  Get and compare dates
+        date_a = df.iloc[[n_row_a]]['session_start_time'].values
+        date_b = df.iloc[[n_row_b]]['session_start_time'].values
+        if date_a.astype('datetime64[D]') != date_b.astype('datetime64[D]'):
+            # Aggregate and append
+            training_time_ab = pd.DataFrame(columns=['sessions'],
+                                            data=df.groupby(['training_status']).size())
+            training_time_b = training_time_b.append(training_time_ab.loc['trained_1b'])
 
 # Training time as a whole
 m_train = training_time_b['sessions'].mean()
